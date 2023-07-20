400b57f3c8bd80f903b29580c5a72a5e5ed2243a
NexusHoncharuk <nexusqwe@iua>
2023-07-20 16:54:43 +0300

    first commit

---
+++ nuxt-dojo/pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <h2>About</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quas magni adipisci atque cumque repudiandae incidunt exercitationem eius harum vitae.</p>
+  <div>{{ data }}</div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch('/api/currency/GBP')
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ nuxt-dojo/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Home</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam quas magni adipisci atque cumque repudiandae incidunt exercitationem eius harum vitae.</p>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
